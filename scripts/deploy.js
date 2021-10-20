ebe5b7839b47a3fdfc62ecddabf452414c13e73b
Nils Lutz <nils.lutz@btc-ag.com>
2021-10-20 20:24:06 +0200

    style: added prettier formatting

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,14 @@
 async function main() {
-  const NimbleNFT = await ethers.getContractFactory("NimbleNFT");
+    const NimbleNFT = await ethers.getContractFactory('NimbleNFT');
 
-  // Start deployment, returning a promise that resolves to a contract object
-  const nimbleNFT = await NimbleNFT.deploy();
-  console.log("Contract deployed to address:", nimbleNFT.address);
+    // Start deployment, returning a promise that resolves to a contract object
+    const nimbleNFT = await NimbleNFT.deploy();
+    console.log('Contract deployed to address:', nimbleNFT.address);
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
